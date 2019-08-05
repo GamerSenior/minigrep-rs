323b78ce3dff18df39b62ea0b2b73db3ff76db1c
Giovani Garcia <giovani.abel@supportweb.com.br>
2019-08-05 18:03:18 -0300

    Enviando mensagens de erro para o standard error.

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problema no parsing de argumentos: {}", err);
+        eprintln!("Problema no parsing de argumentos: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
